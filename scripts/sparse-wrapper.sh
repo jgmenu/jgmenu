01c3ac4bc0fcf3b249aab6c3d72d4d2b5d753e03
Johan Malm <jgm323@gmail.com>
2016-10-13 21:55:23 +0100

    Added scripts/sparse-wrapper.sh

---
+++ scripts/sparse-wrapper.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+makefile_backup=Makefile.sparse
+
+sparse_options="-Wsparse-all -Wno-transparent-union"
+
+die () {
+	echo "fatal: $1"
+	exit 1
+}
+
+
+type sparse >/dev/null 2>&1 || die "you need to install sparse before running this"
+test -e jgmenu.c || die "not run from top level directory of jgmenu repo"
+test -e ${makefile_backup} && die "${makefile_backup} already exists" 
+
+cp -p Makefile ${makefile_backup}
+
+sed --in-place "s/^CC.*/CC = cgcc ${sparse_options}/" Makefile
+
+make clean
+make 2>&1 | grep -v 'glib-autocleanups.h'
+
+cp -pf "${makefile_backup}" Makefile 
+rm -f "${makefile_backup}"
