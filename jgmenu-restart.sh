60831c3e19351cd91e7bf65f8800073d62fa09a7
Johan Malm <jgm323@gmail.com>
2017-02-18 21:31:05 +0000

    jgmenu-restart.sh: fixed bug

--- jgmenu-restart.sh
+++ jgmenu-restart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cmd=$(jgmenu_run config --get csv_cmd)
-test -z ${cmd} && cmd="jgmenu_run parse-pmenu"
+test -z "${cmd}" && cmd="jgmenu_run parse-pmenu"
 
 killall jgmenu
-exec ${cmd} | jgmenu --stay-alive --hide-on-startup
+/bin/sh -c "${cmd} | jgmenu --stay-alive --hide-on-startup"
