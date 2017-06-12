jgmenu/jgmenu

commit e880ddaef46a1a06a292756ee67f6e7ecbe8d129

jgmenu.c: speed up awake
Refactor awake_menu() and hide_menu() in order to speed up wake-up times
on SIGUSR1.

jgmenu.c
@@ -680,18 +680,18 @@ static void if_unity_run_hack(void)
 static void awake_menu(void)
 {
 	if_unity_run_hack();
-	filter_reset();
-	checkout_root();
-	XMapWindow(ui->dpy, ui->win);
-	grabkeyboard();
-	grabpointer();
 	if (config.at_pointer)
 		launch_menu_at_pointer();
+	/* for speed improvement, set tint2_button = 0 */
 	if (config.tint2_button && !config.at_pointer) {
 		tint2env_read_socket();
 		tint2_align();
+		update(1);
 	}
-	update(1);
+	XMapWindow(ui->dpy, ui->win);
+	ui_map_window(geo_get_menu_width(), geo_get_menu_height());
+	grabkeyboard();
+	grabpointer();
 }
 
 static void hide_menu(void)
@@ -699,6 +699,9 @@ static void hide_menu(void)
 	XUngrabKeyboard(ui->dpy, CurrentTime);
 	XUngrabPointer(ui->dpy, CurrentTime);
 	XUnmapWindow(ui->dpy, ui->win);
+	filter_reset();
+	checkout_root();
+	update(1);
 }
 
 void hide_or_exit(void)

commit ef8e4f2ef2750eee1902a2cb45449f8693b39d74

jgmenu.c: fix --at-pointer bug in awake_menu()
Bug introduced in commit e880ddae

jgmenu.c
@@ -700,8 +700,10 @@ static void if_unity_run_hack(void)
 static void awake_menu(void)
 {
 	if_unity_run_hack();
-	if (config.at_pointer)
+	if (config.at_pointer) {
 		launch_menu_at_pointer();
+		resize();
+	}
 	/* for speed improvement, set tint2_button = 0 */
 	if (config.tint2_button && !config.at_pointer) {
 		tint2env_read_socket();