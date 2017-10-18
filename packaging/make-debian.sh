9ed9b6bc50ecbed0ef5904b2dc93ec6f5540a47a
Johan Malm <jgm323@gmail.com>
2017-10-18 21:46:04 +0100

    Update debian files

--- packaging/make-debian.sh
+++ packaging/make-debian.sh
@@ -8,14 +8,13 @@ read answer
 test "$answer" = "y" || exit 1
 test -d "debian" || { echo "fatal: need to run from packacking/"; exit 1; }
 
-# TODO: don't cut, but include commits since last tag + 'dirty'
-#       (this is just to get started)
 ver=$(../scripts/version-gen.sh | sed -e 's/^jgmenu v//' | cut -f1 -d"-")
 dest_dir="jgmenu-${ver}"
+rm -rf jgmenu*
 echo "version ${ver}"
-git describe --exact-match HEAD 2>/dev/null || echo "warn: not at tag"
+#git describe --exact-match HEAD 2>/dev/null || echo "warn: not at tag"
 cd ..
-git archive --format=tar --prefix="${dest_dir}/" HEAD | gzip >packaging/jgmenu_${ver}.orig.tar.gz
+git archive --format=tar --prefix="${dest_dir}/" v${ver} | gzip >packaging/jgmenu_${ver}.orig.tar.gz
 cd packaging
 tar -xf jgmenu_${ver}.orig.tar.gz
 cp -a debian/ ${dest_dir}
