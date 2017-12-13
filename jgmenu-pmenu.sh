3c776a0bf737a661b094bccc9b67aa96ed74efc4
Johan Malm <jgm323@gmail.com>
2017-12-13 21:05:31 +0000

    Remove jgmenu-{pmenu,lx,xdg,ob}*
    These wrappers have been reduced to mere one-liners. They will be
    replaced by their corresponding jgmenu-parse-* csv-command.

--- jgmenu-pmenu.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-(
-jgmenu-parse-pmenu.py --append-file=$HOME/.config/jgmenu/append.csv \
-		      --prepend-file=$HOME/.config/jgmenu/prepend.csv
-) | jgmenu $@
--- jgmenu-xdg.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec jgmenu --csv-cmd="jgmenu_run parse-xdg" $@
