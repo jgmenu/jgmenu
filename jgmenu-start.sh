02d1c299555df71ed054a125fe33dc521f9b514a
Johan Malm <jgm323@gmail.com>
2017-12-14 13:42:35 +0000

    Remove jgmenu-start.sh
    It is simpler to just do 'jgmenu --hide-on-startup'

--- jgmenu-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cmd=$(jgmenu_run config --get csv_cmd)
-test -z "${cmd}" && cmd="jgmenu_run parse-pmenu"
-
-# Run it through a shell enable tilde and variable expansion
-/bin/sh -c "${cmd} | jgmenu --stay-alive --hide-on-startup"
-
