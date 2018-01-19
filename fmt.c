cd897d045e01063e73787d203dc96321853f163a
Johan Malm <jgm323@gmail.com>
2018-01-19 22:11:32 +0000

    lx: support name format (issue #10)
    Set environment variable JGMENU_NAME_FORMAT to format name string
    ("%n %g" by default).
    
    %n - application name
    %g - application generic name in brackets

--- fmt.c
+++ fmt.c
@@ -4,17 +4,52 @@
 #include "sbuf.h"
 #include "fmt.h"
 
+/*
+ * %n - application name
+ * %g - application generic name in brackets
+ */
+static const char default_name_format[] = "%n %g";
+
 void fmt_name(struct sbuf *buf, const char *name, const char *generic_name)
 {
+	static int inited;
+	static char *format;
+	char *p;
+
+	if (!inited) {
+		format = getenv("JGMENU_NAME_FORMAT");
+		if (!format)
+			format = (char *)default_name_format;
+		inited = 1;
+	}
 	sbuf_cpy(buf, "");
-	if (!name)
-		return;
-	sbuf_addstr(buf, name);
-	if (!generic_name)
-		return;
-	if (!strcmp(name, generic_name))
-		return;
-	sbuf_addstr(buf, " (");
-	sbuf_addstr(buf, generic_name);
-	sbuf_addstr(buf, ")");
+	for (p = &format[0]; p; p++) {
+		if (!p || *p == '\0')
+			return;
+		if (*p != '%') {
+			sbuf_addch(buf, p[0]);
+			continue;
+		}
+		switch (*++p) {
+		case '\0':
+			--p;
+			break;
+		case 'n':
+			if (!name)
+				continue;
+			sbuf_addstr(buf, name);
+			break;
+		case 'g':
+			if (!generic_name || !name)
+				continue;
+			if (!strcmp(name, generic_name))
+				continue;
+			sbuf_addstr(buf, " (");
+			sbuf_addstr(buf, generic_name);
+			sbuf_addstr(buf, ")");
+		case '%':
+		default:
+			break;
+		}
+	}
 }
