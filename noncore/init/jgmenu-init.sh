f70f3e5ddffb5f341d88db6aaabff6d5106902cd
Johan Malm <jgm323@gmail.com>
2018-04-15 20:55:10 +0100

    init: remove redundant code (done by 'config')
    With the addition of 'jgmenu_run config', init can be simplified.

--- noncore/init/jgmenu-init.sh
+++ noncore/init/jgmenu-init.sh
@@ -2,7 +2,6 @@
 
 # 'jgmenu init' creates/updates jgmenurc
 
-tmp_jgmenurc=$(mktemp)
 jgmenurc=~/.config/jgmenu/jgmenurc
 jgmenurc_bak=${jgmenurc}.$(date +%Y%m%d%H%M)
 theme=
@@ -11,97 +10,21 @@ regression_items="max_items min_items ignore_icon_cache color_noprog_fg \
 color_title_bg show_title search_all_items ignore_xsettings arrow_show \
 read_tint2rc tint2_rules tint2_button multi_window"
 
-usage () {
-	printf "usage: jgmenu init [<options>]\n"
-	printf "Create/amend config files\n"
-	printf "Options include:\n"
-	printf "    --config-file=<file>  Specify config file\n"
-	printf "    --theme=<theme>       Create config file with a particular theme\n"
-	printf "                          Valid themes include: bunsenlabs\n\n"
+say () {
+	printf "%b\n" "$@"
 }
 
-populate_tmp_file () {
-cat <<'EOF' >>"${tmp_jgmenurc}"
-stay_alive          = 1
-hide_on_startup     = 0
-csv_cmd             = pmenu
-tint2_look          = 1
-at_pointer          = 0
-terminal_exec       = x-terminal-emulator
-terminal_args       = -e
-monitor             = 0
-menu_margin_x       = 0
-menu_margin_y       = 32
-menu_width          = 200
-menu_padding_top    = 5
-menu_padding_right  = 5
-menu_padding_bottom = 5
-menu_padding_left   = 5
-menu_radius         = 1
-menu_border         = 0
-menu_halign         = left
-menu_valign         = bottom
-sub_spacing         = 1
-sub_padding_top     = -1
-sub_padding_right   = -1
-sub_padding_bottom  = -1
-sub_padding_left    = -1
-sub_hover_action    = 1
-item_margin_x       = 3
-item_margin_y       = 3
-item_height         = 25
-item_padding_x      = 4
-item_radius         = 1
-item_border         = 0
-item_halign         = left
-sep_height          = 5
-font                =
-font_fallback       = xtg
-icon_size           = 22
-icon_text_spacing   = 10
-icon_theme          =
-icon_theme_fallback = xtg
-arrow_string        = ▸
-arrow_width         = 15
-color_menu_bg       = #000000 70
-color_menu_fg       = #eeeeee 20
-color_menu_border   = #eeeeee 8
-color_norm_bg       = #000000 00
-color_norm_fg       = #eeeeee 100
-color_sel_bg        = #ffffff 20
-color_sel_fg        = #eeeeee 100
-color_sel_border    = #eeeeee 8
-color_sep_fg        = #ffffff 20
-csv_name_format     = %n (%g)
-EOF
+usage () {
+	say "usage: jgmenu init [<options>]"
+	say "Create/amend config files"
+	say "Options include:"
+	say "    --config-file=<file>  Specify config file"
+	say "    --theme=<theme>       Create config file with a particular theme"
+	say "                          Valid themes include: bunsenlabs\n"
 }
 
 backup_jgmenurc () {
 	test -e ${jgmenurc} && cp -p ${jgmenurc} ${jgmenurc_bak}
-}
-
-print_start_msg () {
-	if test -z ${printed+x}
-	then
-		printf "%b\n" "Add missing items to config file..."
-		printf "\n\n%b\n\n" "### the items below were added by 'jgmenu_run init'" >> ${jgmenurc}
-	fi
-	printed=1
-}
-
-amend_jgmenurc () {
-	local prefix="      - "
-	while IFS= read -r line
-	do
-		key=$(echo ${line%%=*} | tr -d ' ')
-		test -z "${key}" && continue
-		if ! grep "^${key}[\ =]\|[\ #]${key}[\ =]" "${jgmenurc}" >/dev/null 2>&1
-		then
-			print_start_msg
-			printf "${prefix}%b\n" "${line}"
-			printf "#%b\n" "${line}" >> ${jgmenurc}
-		fi
-	done <${tmp_jgmenurc}
 }
 
 # START OF SCRIPT
@@ -129,9 +52,9 @@ done
 mkdir -p ~/.config/jgmenu
 backup_jgmenurc
 
+JGMENU_EXEC_DIR=$(jgmenu_run --exec-path)
 if ! test -z ${theme}
 then
-	JGMENU_EXEC_DIR=$(jgmenu_run --exec-path)
 	if test "${theme}" = "bunsenlabs"
 	then
 		. ${JGMENU_EXEC_DIR}/jgmenu-init--bunsenlabs.sh
@@ -140,9 +63,13 @@ then
 		printf "warn: cannot find theme '${theme}'\n"
 	fi
 else
-	populate_tmp_file
-	amend_jgmenurc
-	rm -f ${tmp_jgmenurc}
+	if ! test -e ${jgmenurc}
+	then
+		say "info: creating jgmenurc"
+		cp ${JGMENU_EXEC_DIR}/jgmenurc ${jgmenurc}
+	else
+		jgmenu_run config amend
+	fi
 fi
 
 # Check for jgmenurc items which are no longer valid
