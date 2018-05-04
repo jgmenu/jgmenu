08b6bba331f167b66e116b3dd787ef3bf0b27801
Johan Malm <jgm323@gmail.com>
2018-05-04 21:48:21 +0100

    init: add browser in prepend script

--- noncore/init/jgmenu-init--prepend.sh
+++ noncore/init/jgmenu-init--prepend.sh
@@ -1,5 +1,29 @@
 # This shell script fragment is designed to be sourced by jgmenu-init.sh
 
+prepend_add () {
+	printf "%b\n" "$@" >>"${prepend_file}"
+}
+
+add_browser () {
+	test -e ${prepend_file} && for b in ${browsers}
+	do
+		if grep -i ${b} ${prepend_file} >/dev/null 2>&1
+		then
+			say "prepend.csv already contains a browser entry"
+			return
+		fi
+	done
+	for b in ${browsers}
+	do
+		if type ${t} >/dev/null 2>&1
+		then
+			prepend_add "Browser,${b},${b}"
+			printf "Prepend %b\n" "${b}"
+			break
+		fi
+	done
+}
+
 add_terminal () {
 	test -e ${prepend_file} && for t in ${terminals}
 	do
@@ -13,17 +37,16 @@ add_terminal () {
 	do
 		if type ${t} >/dev/null 2>&1
 		then
-			printf "%b\n" "Terminal,${t},utilities-terminal" \
-				>>"${prepend_file}"
-			say "'${t}' added"
+			prepend_add "Terminal,${t},utilities-terminal"
+			printf "Prepend %b\n" "${t}"
 			break
 		fi
 	done
 }
 
 prepend_items () {
+	#File manager,pcmanfm,system-file-manager
+	add_browser
 	add_terminal
-#Browser,firefox,firefox
-#File manager,pcmanfm,system-file-manager
-#Terminal,xterm,utilities-terminal
+	prepend_add "^sep()"
 }
