cc075a6bcd90b96ebfa962442fd1c874765f0282
Johan Malm <jgm323@gmail.com>
2018-05-05 20:23:43 +0100

    init: add file manager in prepend script

--- noncore/init/jgmenu-init--prepend.sh
+++ noncore/init/jgmenu-init--prepend.sh
@@ -5,53 +5,75 @@ lxterminal qterminal urxvt rxvt xfce4-terminal konsole sakura st"
 
 prepend__browsers="firefox iceweasel chromium midori"
 
+prepend__file_managers="pcmanfm thunar nautilus caja"
+
 prepend__add () {
 	printf "%b\n" "$@" >>"${prepend_file}"
 }
 
-prepend__add_browser () {
-	test -e ${prepend_file} && for b in ${prepend__browsers}
-	do
-		if grep -i ${b} ${prepend_file} >/dev/null 2>&1
-		then
-			say "prepend.csv already contains a browser entry"
-			return
-		fi
-	done
-	for b in ${prepend__browsers}
-	do
-		if type ${t} >/dev/null 2>&1
-		then
-			prepend__add "Browser,${b},${b}"
-			printf "Prepend %b\n" "${b}"
-			break
-		fi
-	done
-}
-
 prepend__add_terminal () {
-	test -e ${prepend_file} && for t in ${prepend__terminals}
+	test -e ${prepend_file} && for x in ${prepend__terminals}
 	do
-		if grep -i ${t} ${prepend_file} >/dev/null 2>&1
+		if grep -i ${x} ${prepend_file} >/dev/null 2>&1
 		then
 			say "prepend.csv already contains a terminal entry"
 			return
 		fi
 	done
-	for t in ${prepend__terminals}
+	for x in ${prepend__terminals}
 	do
-		if type ${t} >/dev/null 2>&1
+		if type ${x} >/dev/null 2>&1
 		then
-			prepend__add "Terminal,${t},utilities-terminal"
-			printf "Prepend %b\n" "${t}"
+			prepend__add "Terminal,${x},utilities-terminal"
+			printf "Prepend %b\n" "${x}"
+			break
+		fi
+	done
+}
+
+prepend__add_browser () {
+	test -e ${prepend_file} && for x in ${prepend__browsers}
+	do
+		if grep -i ${x} ${prepend_file} >/dev/null 2>&1
+		then
+			say "prepend.csv already contains a browser entry"
+			return
+		fi
+	done
+	for x in ${prepend__browsers}
+	do
+		if type ${x} >/dev/null 2>&1
+		then
+			prepend__add "Browser,${x},${x}"
+			printf "Prepend %b\n" "${x}"
+			break
+		fi
+	done
+}
+
+prepend__add_file_manager () {
+	test -e ${prepend_file} && for x in ${prepend__file_managers}
+	do
+		if grep -i ${x} ${prepend_file} >/dev/null 2>&1
+		then
+			say "prepend.csv already contains a file manager entry"
+			return
+		fi
+	done
+	for x in ${prepend__file_managers}
+	do
+		if type ${x} >/dev/null 2>&1
+		then
+			prepend__add "File manager,${x},system-file-manager"
+			printf "Prepend %b\n" "${x}"
 			break
 		fi
 	done
 }
 
 prepend_items () {
-	#File manager,pcmanfm,system-file-manager
-	prepend__add_browser
 	prepend__add_terminal
+	prepend__add_browser
+	prepend__add_file_manager
 	prepend__add "^sep()"
 }
