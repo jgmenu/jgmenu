13fcb48c8bf568f9b723ee130861d33cb4ecc209
Johan Malm <jgm323@gmail.com>
2018-06-30 21:04:26 +0100

    Simplify scripts/clock-wrapper.sh

--- scripts/cloc-wrapper.sh
+++ scripts/cloc-wrapper.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-versions="master v0.8 v0.7 v0.6 v0.5 v0.4 v0.3 v0.2 v0.1.2"
-
-for v in $versions
-do
-	git checkout "${v}"
-	printf "%b\n" "version ${v}"
-	cloc . -not-match-d="tests noncore" \
-		| grep '^C \|^Bourne\|^Python' \
-		| awk '{ print $1, $5 }' 2>/dev/null
-done
+cloc . -not-match-d="tests noncore" \
+	| grep '^C \|^Bourne\|^Python' \
+	| awk '{ print $1, $5 }' 2>/dev/null
