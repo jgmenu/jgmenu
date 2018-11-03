e48fe5206594363645b082f430774cc10513e45f
Johan Malm <jgm323@gmail.com>
2018-11-03 16:39:28 +0000

    ob: add unit test for --cmd= command

--- tests/t1100-ob.sh
+++ tests/t1100-ob.sh
@@ -9,7 +9,16 @@ test_ob() {
 	test_cmp expect actual
 }
 
-test_expect_success 'test openbox menu1' '
+test_ob_cmd() {
+	cat t1100/"$2" >expect
+	../jgmenu-ob --cmd="cat t1100/$1" >actual
+	test_cmp expect actual
+}
+
+test_expect_success 'test openbox module with file input (menu1.xml)' '
 test_ob "menu1.xml" "menu1.csv"
 '
 
+test_expect_success 'test openbox module with command input (cat menu1.xml)' '
+test_ob_cmd "menu1.xml" "menu1.csv"
+'
