e217d9f89616da13eb80ed0cc4b125a6dd46ae3a
Johan Malm <jgm323@gmail.com>
2018-11-10 16:03:07 +0000

    i18n: add unit test t1101

---
+++ tests/t1101-i18n.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+test_description='test i18n'
+. ./test-lib-simple.sh
+
+# $1: menu file
+# $2: po file
+# $3: translated file
+test_i18n() {
+	cat t1101/"$3" >expect
+	cat t1101/"$1" | ../jgmenu-i18n t1101/"$2" >actual
+	test_cmp expect actual
+}
+
+
+test_expect_success 'test i18n module with Swedish po file' '
+test_i18n "menu" "sv" "menu.sv"
+'
+
---
+++ tests/t1101/menu.sv
@@ -0,0 +1,22 @@
+Terminal,^term(bash)
+Filhanterare,pcmanfm
+Webbläsare,firefox
+Textredigerare,^term(vim)
+^sep()
+Tillbehör,^checkout(accessories)
+^sep()
+Platser,^pipe(jgmenu_run places)
+^sep()
+Hjälp och Information,^checkout(help)
+^sep()
+Lås skärmen,i3lock-fancy -p
+
+^tag(accessories)
+Post,:
+Meddelande,:
+Fildelning,:
+Skärmbild,:
+
+^tag(help)
+Hjälp,:
+Information,:
