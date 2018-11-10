6f557724e3c0402f9351806e08e608543db199a0
Johan Malm <jgm323@gmail.com>
2018-11-10 15:54:13 +0000

    i18n: add --init

--- jgmenu-i18n.c
+++ jgmenu-i18n.c
@@ -17,13 +17,45 @@
 
 static const char jgmenu_i18n_usage[] =
 "Usage: jgmenu_run i18n <translation file>\n"
+"       jgmenu_run i18n --init\n\n"
+"Options:\n"
+"    --init                print missing msgid entries for po file\n\n"
 "Example:\n"
-"jgmenu_run ob | jgmenu_run i18n sv.po | jgmenu --simple\n";
+" 1) jgmenu_run ob | jgmenu_run i18n --init >sv\n"
+" 2) Translate entries in po file.\n"
+" 3) jgmenu_run ob | jgmenu_run i18n sv | jgmenu --simple\n";
+
+static int arg_init;
 
 void usage(void)
 {
 	printf("%s", jgmenu_i18n_usage);
 	exit(0);
+}
+
+static int is_reserved_word(const char *buf)
+{
+	if (buf[0] == '^')
+		return 1;
+	return 0;
+}
+
+static void create_msgid_if_missing(char *buf)
+{
+	char *p, *translation;
+
+	BUG_ON(!buf);
+	if (buf[0] == '\0')
+		return;
+	if (is_reserved_word(buf))
+		return;
+	p = strchr(buf, ',');
+	if (p)
+		*p = '\0';
+	translation = i18n_translate(buf);
+	if (translation)
+		return;
+	printf("msgid \"%s\"\nmsgstr \"\"\n\n", buf);
 }
 
 static void translate_and_print(char *buf)
@@ -53,6 +85,8 @@ int main(int argc, char **argv)
 			if (argc > i + 1)
 				die("<file> must be the last argument");
 			break;
+		} else if (!strcmp(argv[i], "--init")) {
+			arg_init = 1;
 		} else if (!strcmp(argv[i], "--help")) {
 			usage();
 		}
@@ -70,7 +104,10 @@ int main(int argc, char **argv)
 			*p = '\0';
 		else
 			die("line %d not correctly terminated", i);
-		translate_and_print(buf);
+		if (arg_init)
+			create_msgid_if_missing(buf);
+		else
+			translate_and_print(buf);
 	}
 	i18n_cleanup();
 	return 0;
