366853c884e62fc4ca35dfa96d43ba4107f48f44
Johan Malm <jgm323@gmail.com>
2018-12-20 21:26:34 +0000

    scripts/sparse-wrapper.sh: refactor and simplify

--- scripts/sparse-wrapper.sh
+++ scripts/sparse-wrapper.sh
@@ -1,25 +1,6 @@
 #!/bin/sh
 
-makefile_backup=Makefile.sparse
-
-sparse_options="-Wsparse-all -Wno-transparent-union"
-
-die () {
-	echo "fatal: $1"
-	exit 1
-}
-
-
-type sparse >/dev/null 2>&1 || die "you need to install sparse before running this"
 test -e jgmenu.c || die "not run from top level directory of jgmenu repo"
-test -e ${makefile_backup} && die "${makefile_backup} already exists" 
-
-cp -p Makefile.inc ${makefile_backup}
-
-sed --in-place "s/^CC.*/CC = cgcc ${sparse_options}/" Makefile.inc
-
-make clean
-make 2>&1 | grep -v 'glib-autocleanups.h'
-
-cp -pf "${makefile_backup}" Makefile.inc
-rm -f "${makefile_backup}"
+export CC="cgcc"
+export CFLAGS="-Wsparse-all -Wno-transparent-union"
+make
