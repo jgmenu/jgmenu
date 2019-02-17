f8a9773fab70d0e4494b269e14726fbd3a0e47a5
Johan Malm <jgm323@gmail.com>
2019-02-17 20:30:27 +0000

    Rename tests/t0006.. examples/ex09.sh

--- examples/ex09.sh
+++ examples/ex09.sh
@@ -4,12 +4,12 @@ menu_file=
 
 ask_user_for_input () {
 	tmp_file=$(mktemp)
-	cd t0006/menus
+	cd ../tests/t0006/menus
 	for i in ./*
 	do
 		echo "${i#./},${i#./}" >> ${tmp_file}
 	done
-	menu_file=$(cat ${tmp_file} | jgmenu --no-spawn --config-file=t0006-jgmenurc --simple 2>/dev/null)
+	menu_file=$(cat ${tmp_file} | jgmenu --no-spawn --config-file= --simple 2>/dev/null)
 	cd ../..
 	rm -f ${tmp_file}
 }
@@ -17,5 +17,5 @@ ask_user_for_input () {
 ask_user_for_input
 export XDG_MENU_PREFIX="${menu_file%applications\.menu}"
 printf "XDG_MENU_PREFIX=%b\n" "${XDG_MENU_PREFIX}"
-export XDG_CONFIG_DIRS="a:b:c:::d:$PWD/t0006:e:f"
-../jgmenu --csv-cmd="$PWD/../jgmenu-xdg" --simple
+export XDG_CONFIG_DIRS="a:b:c:::d:$PWD/../tests/t0006:e:f"
+../jgmenu --csv-cmd="$PWD/../jgmenu-lx" --simple --config-file=
