e2d07f904d66c7ab44d99bb4499827abee9980b7
Johan Malm <jgm323@gmail.com>
2019-07-06 15:57:24 +0100

    Remove scripts/pylint-wrapper.sh

--- scripts/pylint-wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# --variable-rgx allows variable length of 1 (for i, j, x, etc)
-
-pylint \
-	--variable-rgx='[a-z_][a-z0-9_]{0,30}$' \
-	--module-rgx='jgmenu-config' \
-	./noncore/config/jgmenu-config.py
