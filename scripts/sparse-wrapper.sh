e41037fcdad6c60e616321aaf620f38446cf4c21
Johan Malm <jgm323@gmail.com>
2019-09-19 20:54:34 +0100

    scripts/sparse-wrapper.sh: minor fix

--- scripts/sparse-wrapper.sh
+++ scripts/sparse-wrapper.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-test -e jgmenu.c || die "not run from top level directory of jgmenu repo"
+if ! test -e src/jgmenu.c ; then
+	printf '%b\n' "not run from top level directory of jgmenu repo"
+	exit 1
+fi
+
 export CC="cgcc"
 export CFLAGS="-Wsparse-all -Wno-transparent-union"
+
 make
