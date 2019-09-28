44d01316b47d3574255080b40118982f62e87706
Johan Malm <jgm323@gmail.com>
2019-09-28 21:33:53 +0100

    mate-panel: support ipc mode

--- contrib/mate-panel/mate-jgmenu.py
+++ contrib/mate-panel/mate-jgmenu.py
@@ -8,137 +8,72 @@ from gi.repository import Gtk, Gdk
 from gi.repository import MatePanelApplet
 from gi.repository.GdkPixbuf import Pixbuf
 import os
-import sys
 import shlex
 import subprocess
-import re
 import setproctitle
 
 # Rename the process
 setproctitle.setproctitle('mate-jgmenu')
 
-HOME=os.environ['HOME']
-CONFIG_PATH=HOME + '/.config/jgmenu/jgmenurc'
-
-
 class JgMenu(MatePanelApplet.Applet):
     def __init__(self, applet):
         self.applet_fill(applet)
 
-
     def execute(self, command):
-        """function to exec"""
-        subprocess.Popen(shlex.split(command), 
+        subprocess.Popen(shlex.split(command),
                          stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     def applet_fill(self, applet):
-
          # you can use this path with gio/gsettings
         settings_path = applet.get_preferences_path()
-
         applet.set_border_width(0)
         self.button = Gtk.Button(border_width=0)
         try:
-           pixbuf = Gtk.IconTheme.get_default().load_icon("distributor-logo", applet.get_size(), 0)
+           pixbuf = Gtk.IconTheme.get_default().load_icon("start-here", applet.get_size(), 0)
            button_icon = Gtk.Image.new_from_pixbuf(pixbuf)
         except:
            button_icon = Gtk.Label("jgmenu")
         self.button.add(button_icon)
         # Disable label to show icon
-        self.button.set_label("JG menu")
+#        self.button.set_label("jgmenu")
         self.button.connect("clicked", self.on_toggle, self.button, applet)
         self.applet = applet
         self.applet.add(self.button)
         toplevel = self.button.get_toplevel()
-
         self.applet.show_all()
 
-
-    def replace(self, file, pattern, subst):
-        file_handle = open(file, 'r')
-        file_string = file_handle.read()
-        file_handle.close()
-        file_string = (re.sub(pattern, subst, file_string))
-        file_handle = open(file, 'w')
-        file_handle.write(file_string)
-        file_handle.close()
-
-    def parse_config(self, filename):
-        COMMENT_CHAR = '#'
-        OPTION_CHAR =  '='
-        options = {}
-        f = open(filename)
-        for line in f:
-            if COMMENT_CHAR in line:
-                line, comment = line.split(COMMENT_CHAR, 1)
-            if OPTION_CHAR in line:
-                option, value = line.split(OPTION_CHAR, 1)
-                option = option.strip()
-                value = value.strip()
-                options[option] = value
-        f.close()
-        return options
-
     def on_toggle(self, button, applet, data=None):
+        # make sure we're in ipc mode - won't touch jgmenurc if already in ipc
+        self.execute("jgmenu_run config -s {} -k position_mode -v ipc".format(os.environ["HOME"] + '/.config/jgmenu/jgmenurc'))
+
         rect = button.get_allocation()
         main_window = self.button.get_toplevel()
-        [val, win_x, win_y] = main_window.get_window().get_origin()
+        [val, button_x1, button_y1] = main_window.get_window().get_origin()
+        button_x2 = button_x1 + rect.width
+        button_y2 = button_y1 + rect.height
+        button_y1=0
         orientation = self.applet.get_orient()
-        # This needs fixing
-        if orientation == 0:
-           # BOTTOM
-           cal_x = win_x 
-           cal_y = win_y
-        elif orientation == 1:
-           # TOP
-           cal_x = win_x + rect.x
-           cal_y = win_y + rect.y + rect.height
-           print(cal_y)
-        elif orientation == 2:
-           # RIGHT
-           cal_x = win_x
-           cal_y = win_y
-        elif orientation == 3:
-           # LEFT
-           cal_x = win_x + rect.x
-           cal_y = win_y + rect.y
-        else:
-           cal_x = win_x
-           cal_y = win_y
-        options = self.parse_config(CONFIG_PATH)
-        [x, y] = self.apply_screen_coord_correction(cal_x, cal_y, options, button)
-        #set coordinates
-        pattern_x = re.compile("menu_margin_x\s+\=\s+(\d+)")
-        self.replace(CONFIG_PATH,pattern_x,'menu_margin_x = ' + str(x))
-        pattern_y = re.compile("menu_margin_y\s+\=\s+(\d+)")
-        self.replace(CONFIG_PATH,pattern_y,'menu_margin_y = ' + str(y))
-        self.execute('jgmenu')
 
-    def apply_screen_coord_correction(self, x, y, options, relative_widget):
-        corrected_y = y
-        corrected_x = x
+        # Cheeky hack to watch a few variables
+        f = open("/home/johan/mate-temp", 'w')
+        f.write("x={} ".format(button_x2))
+        f.write("y={} ".format(button_y2))
+        f.write("\n")
+        f.close()
 
-        screen_w = Gdk.Screen.width()
-        screen_h = Gdk.Screen.height()
-        delta_x = screen_w - (x + int(options["menu_width"]))
-        delta_y = screen_h - (y + int(options["menu_height_max"]))
-        corrected_y = delta_y
-        print(delta_x,delta_y)
-        if delta_x < 0:
-            corrected_x += delta_x
-        if corrected_x < 0:
-            corrected_x = 0
-        if delta_y < 0:
-            corrected_y = y - int(options["menu_height_max"]) - relative_widget.get_allocation().height
-        if corrected_y < 0:
-            corrected_y = 0
-        print(corrected_x,corrected_y)
-        return [corrected_x, corrected_y]
-
+        os.environ["TINT2_BUTTON_PANEL_X1"] = "0"
+        os.environ["TINT2_BUTTON_PANEL_Y1"] = "0"
+        os.environ["TINT2_BUTTON_PANEL_X2"] = "1024"
+        os.environ["TINT2_BUTTON_PANEL_Y2"] = "{}".format(button_y2)
+        os.environ["TINT2_BUTTON_ALIGNED_X1"] = "{}".format(button_x1)
+        os.environ["TINT2_BUTTON_ALIGNED_Y1"] = "{}".format(button_y2)
+        os.environ["TINT2_BUTTON_ALIGNED_X2"] = "{}".format(button_x2)
+        os.environ["TINT2_BUTTON_ALIGNED_Y2"] = "{}".format(button_y2)
+        self.execute("jgmenu_run")
 
 def applet_factory(applet, iid, data):
     if iid != "JgMenuApplet":
-       return False
+        return False
     JgMenu(applet)
     return True
 
