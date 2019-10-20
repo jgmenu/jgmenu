a2f60b2e9d43fe578644d799bf7ca3805b5086e0
Jens John <dev@2ion.de>
2019-10-20 13:55:15 +0200

    Add docker-build.sh universal build wrapper

---
+++ docker-build.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+DOCKER_IMAGE=$DOCKER_IMAGE
+
+if [[ -z "$DOCKER_IMAGE" ]]; then
+	CHANGELOG_HEAD=$(head -n1 debian/changelog)
+	case "$CHANGELOG_HEAD" in
+		*lithium*|*buster*) 	DOCKER_IMAGE=debian:buster ;;
+		*helium*|*stretch*) 	DOCKER_IMAGE=debian:stretch ;;
+		*hydrogen*|*jessie*)	DOCKER_IMAGE=debian:jessie ;;
+	esac
+	if (( $USE_I386 = 1 )); then
+		DOCKER_IMAGE="i386/$DOCKER_IMAGE"
+	fi
+fi
+
+echo "DOCKER IMAGE: $DOCKER_IMAGE"
+
+sudo rm -rf -- "$PWD"/build
+sudo docker run -v "$PWD:/mnt" "${DOCKER_IMAGE}" /bin/bash -c '
+  set -e;
+
+  echo -ne "\e[0;34m";
+  source /etc/os-release;
+  cat >/etc/apt/sources.list.d/sources.list <<<"
+deb-src http://deb.debian.org/debian/ ${VERSION_CODENAME} main contrib non-free
+deb-src http://security.debian.org/ ${VERSION_CODENAME}/updates main contrib non-free";
+  apt-get update && apt-get -yy upgrade;
+  apt-get install -yy debhelper devscripts lsb-release rsync git;
+
+  echo "Copying source...";
+  mkdir -p /tmp/build;
+  rsync --archive /mnt/ /tmp/build/;
+
+  echo "Building..."; set -x;
+  cd /tmp/build/;
+  yes | mk-build-deps -i debian/control; rm jgmenu-build-deps*.deb;
+  echo -ne "\e[0m";
+  lastref=$(git describe --tags --abbrev=0);
+  lastref=${lastref%-*};
+  gitbranch=$(git rev-parse --abbrev-ref HEAD);
+  pkgbase=$(grep Source: debian/control|cut -d" " -f2);
+  git archive --format=tgz "$gitbranch" > ../"${pkgbase}_${lastref}.orig.tar.gz";
+  dpkg-buildpackage -F -rfakeroot -us -uc;
+  echo -ne "\e[0;34m"; set +x;
+
+  echo "Copying results...";
+  arch=$(dpkg --print-architecture);
+  mkdir -p /mnt/build/$arch;
+  rsync /tmp/*.{deb,dsc,tar.*,changes} /mnt/build/$arch;
+'
