e559fbf413a19db2a140cb379aad2415c5395559
Johan Malm <jgm323@gmail.com>
2019-12-08 17:54:34 +0000

    tests/helper/tests-dirs.c: update

--- tests/helper/test-dirs.c
+++ tests/helper/test-dirs.c
@@ -6,7 +6,7 @@
 int main(int argc, char **argv)
 {
 	struct dir *dir, *dirs;
-       
+
 	dirs_read_schema(&dirs);
 	for (dir = dirs; dir->name; dir += 1)
 		printf("%s,%s\n", dir->name, dir->icon);
