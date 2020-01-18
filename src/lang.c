b81f9544a83db712f5763442e73e17cc2058d8f5
Johan Malm <jgm323@gmail.com>
2020-01-18 16:35:50 +0000

    lang.c: fix coding style

--- src/lang.c
+++ src/lang.c
@@ -6,12 +6,16 @@
 #include "compat.h"
 #include "util.h"
 
-static char _ll[32] = { 0 };
-static char _llcc[32] = { 0 };
-static char _name_ll[32] = { 0 }; /* Name[ll] */
-static char _name_llcc[32] = { 0 }; /* Name[ll_CC] */
-static char _gname_ll[32] = { 0 }; /* GenericName[ll] */
-static char _gname_llcc[32] = { 0 }; /* GenericName[ll_CC] */
+static char ll[24] = { 0 };
+static char llcc[24] = { 0 };
+
+/*
+ * Static name keys for GenericName[ll_CC], etc
+ */
+static char name_ll[64] = { 0 };
+static char name_llcc[64] = { 0 };
+static char gname_ll[64] = { 0 };
+static char gname_llcc[64] = { 0 };
 
 static void lang_init(void)
 {
@@ -29,43 +33,43 @@ static void lang_init(void)
 	}
 
 	/* ll_CC */
-	strlcpy(_llcc, p, sizeof(_llcc));
-	p = strrchr(_llcc, '.');
+	strlcpy(llcc, p, sizeof(llcc));
+	p = strrchr(llcc, '.');
 	if (p)
 		*p = '\0';
 
 	/* ll */
-	strlcpy(_ll, _llcc, sizeof(_ll));
-	p = strrchr(_ll, '_');
+	strlcpy(ll, llcc, sizeof(ll));
+	p = strrchr(ll, '_');
 	if (p)
 		*p = '\0';
 
-	snprintf(_name_ll, sizeof(_name_ll), "Name[%s]", _ll);
-	snprintf(_name_llcc, sizeof(_name_llcc), "Name[%s]", _llcc);
-	snprintf(_gname_ll, sizeof(_gname_ll), "GenericName[%s]", _ll);
-	snprintf(_gname_llcc, sizeof(_gname_llcc), "GenericName[%s]", _llcc);
+	snprintf(name_ll, sizeof(name_ll), "Name[%s]", ll);
+	snprintf(name_llcc, sizeof(name_llcc), "Name[%s]", llcc);
+	snprintf(gname_ll, sizeof(gname_ll), "GenericName[%s]", ll);
+	snprintf(gname_llcc, sizeof(gname_llcc), "GenericName[%s]", llcc);
 }
 
 char *lang_name_ll(void)
 {
 	lang_init();
-	return _name_ll;
+	return name_ll;
 }
 
 char *lang_name_llcc(void)
 {
 	lang_init();
-	return _name_llcc;
+	return name_llcc;
 }
 
 char *lang_gname_ll(void)
 {
 	lang_init();
-	return _gname_ll;
+	return gname_ll;
 }
 
 char *lang_gname_llcc(void)
 {
 	lang_init();
-	return _gname_llcc;
+	return gname_llcc;
 }
