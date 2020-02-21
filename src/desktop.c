jgmenu/jgmenu

commit 19e7d99d7270e8fceacc0469280b641fbb80334a

Strip .desktop exec field codes better (issue #68)
Remove field codes (e.g. %U and %f) from .desktop file Exec= strings, but
respect that % can be escaped using %%.

Reported-by: @Vladimir-csp

src/desktop.c
@@ -23,19 +23,6 @@
 static struct app *apps;
 static int nr_apps, alloc_apps;
 
-/* TODO: parse this properly and share code with lx */
-static void format_exec(char *s)
-{
-	char *p;
-
-	if (!s)
-		return;
-	/* Remove %U, %f, etc at the end of Exec cmd */
-	p = strchr(s, '%');
-	if (p)
-		*p = '\0';
-}
-
 static void parse_line(char *line, struct app *app, int *is_desktop_entry)
 {
 	char *key, *value;
@@ -155,7 +142,7 @@ static int add_app(FILE *fp, char *filename)
 			return -1;
 		parse_line(line, app, &is_desktop_entry);
 	}
-	format_exec(app->exec);
+	strip_exec_field_codes(&app->exec);
 	app->filename = strdup(filename);
 	strdup_null_variables(app);
 	return 0;

src/util.c
@@ -97,23 +97,25 @@ void *xcalloc(size_t nb, size_t size)
 	return ret;
 }
 
-char *strstrip(char *s)
+static void rtrim(char **s)
 {
 	size_t len;
 	char *end;
 
-	len = strlen(s);
+	len = strlen(*s);
 	if (!len)
-		return s;
-
-	end = s + len - 1;
-	while (end >= s && isspace(*end))
+		return;
+	end = *s + len - 1;
+	while (end >= *s && isspace(*end))
 		end--;
 	*(end + 1) = '\0';
+}
 
+char *strstrip(char *s)
+{
+	rtrim(&s);
 	while (isspace(*s))
 		s++;
-
 	return s;
 }
 
@@ -309,4 +311,21 @@ void msleep(unsigned int duration)
 	ts.tv_sec  = sec;
 	ts.tv_nsec =  msec * 1000000;
 	nanosleep(&ts, NULL);
+}
+
+void strip_exec_field_codes(char **exec)
+{
+	char *p = *exec;
+
+	for (; *p; p++) {
+		if (*p == '%') {
+			*p = ' ';
+			++p;
+			if (*p == '\0')
+				break;
+			if (*p != '%')
+				*p = ' ';
+		}
+	}
+	rtrim(exec);
 }

src/util.h
@@ -43,4 +43,11 @@ void remove_caret_markup_closing_bracket(char *s);
 void mkdir_p(const char *path);
 void msleep(unsigned int duration);
 
+/**
+ * strip_exec_field_codes - remove field codes from .desktop file exec field
+ * @exec - pointer to exec string
+ * Remove all %? except %% which becomes %
+ */
+void strip_exec_field_codes(char **exec);
+
 #endif /* UTIL_H */

commit dbc153f094b3da46b80a89afcdc42936d2ef8e5e

apps: fix segfault (issue #112)
Bug introduced in commit 19e7d99d and manifests itself if a .desktop
file contains no Exec= field

src/desktop.c
@@ -142,9 +142,9 @@ static int add_app(FILE *fp, char *filename)
 			return -1;
 		parse_line(line, app, &is_desktop_entry);
 	}
-	strip_exec_field_codes(&app->exec);
 	app->filename = strdup(filename);
 	strdup_null_variables(app);
+	strip_exec_field_codes(&app->exec);
 	return 0;
 }
 

src/util.c
@@ -315,9 +315,11 @@ void msleep(unsigned int duration)
 
 void strip_exec_field_codes(char **exec)
 {
-	char *p = *exec;
+	char *p;
 
-	for (; *p; p++) {
+	if (!**exec || !*exec)
+		return;
+	for (p = *exec; *p; p++) {
 		if (*p == '%') {
 			*p = ' ';
 			++p;