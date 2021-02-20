6bd4a9fea6a1b663a9fb67d979bf8aa91f7504cf
Johan Malm <jgm323@gmail.com>
2021-02-20 10:07:29 +0000

    apps: make exec[] field bigger to allow long strings for wine

--- src/desktop.h
+++ src/desktop.h
@@ -10,11 +10,11 @@ struct app {
 	char name_localized[128];
 	char generic_name[128];
 	char generic_name_localized[128];
-	char exec[128];
-	char tryexec[128];
+	char exec[1024];
+	char tryexec[1024];
 	bool tryexec_not_in_path;
 	char icon[128];
-	char categories[512];
+	char categories[1024];
 	bool nodisplay;
 	char filename[128];
 	bool terminal;
