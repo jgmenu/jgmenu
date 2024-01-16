f3425b699156fea81a016fa99f17ce43ea1ebc4d
tunalad <28594843+tunalad@users.noreply.github.com>
2024-01-16 23:05:34 +0100

    updated jgmenu_run, replaced `#define LOCKFILE`

--- src/lockfile.c
+++ src/lockfile.c
@@ -8,9 +8,6 @@
 #include "sbuf.h"
 #include "banned.h"
 
-//#define LOCKFILE "~/.jgmenu-lockfile"
-#define LOCKFILE get_lockfile()
-
 const char* get_lockfile() {
         const char *xdg_runtime_dir = getenv("XDG_RUNTIME_DIR");
         static char lockfile_path[PATH_MAX];
@@ -35,13 +32,13 @@ void lockfile_init(void)
 	struct stat st;
 
 	sbuf_init(&lockfile);
-	sbuf_cpy(&lockfile, LOCKFILE);
+	sbuf_cpy(&lockfile, get_lockfile());
 	sbuf_expand_tilde(&lockfile);
 	if (!stat(lockfile.buf, &st)) {
 		die("Lockfile '%s' exists. This is probably because another\n"
 		    "       instance of jgmenu is running. If this is not"
 		    " the case, manually delete\n       the lockfile.",
-		     LOCKFILE);
+		     get_lockfile());
 	}
 	fd = open(lockfile.buf, O_RDWR | O_CREAT, 0600);
 	if (fd < 0)
